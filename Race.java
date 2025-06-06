4845c9466827ff70d966d80de1a9b4d69b8d750e
Abhay Ramkrushna Patil <patilabhay484@gmail.com>
2025-06-06 17:05:39 +0530

    var sealed

--- Race.java
+++ Race.java
@@ -1,30 +1,57 @@
-class increment{
+// class increment{
 
-    int count = 0 ;
-    public synchronized void increment(){
+//     int count = 0 ;
+//     public synchronized void increment(){
 
-        for( int i = 0 ; i < 1000 ; i++ ){
-            count ++ ;
-        }
+//         for( int i = 0 ; i < 1000 ; i++ ){
+//             count ++ ;
+//         }
+//     }
+// }
+
+// public class Race {
+    
+//     public static void main(String[] args) throws InterruptedException {
+        
+//         increment i = new increment();
+//         Thread t1 = new Thread(() ->{
+//             i.increment();
+//         });
+        
+//         Thread t2 = new Thread(() ->{
+//             i.increment();
+//         });
+
+//         t1.start();
+//         t2.start();
+//         t1.join();
+//         t2.join();
+//         System.out.println(i.count);
+
+//         var n = 10 ;
+//         System.out.println(n);
+//     }
+// }
+
+sealed class A permits B,C {
+
+    A(){
+        System.out.println("A");
     }
 }
+final class B extends A{
 
-public class Race {
-    
-    public static void main(String[] args) {
-        
-        increment i = new increment();
-        Thread t1 = new Thread(() ->{
-            i.increment();
-        });
-        
-        Thread t2 = new Thread(() ->{
-            i.increment();
-        });
+}
+final class C extends A{
 
-        t1.start();
-        t2.start();
-        System.out.println(i.count);
+}
+class D{
 
+}
+
+class Race{
+
+    public static void main(String[] args ){
+        B b = new B();
     }
 }
