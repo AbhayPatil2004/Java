9989b5c4fd5738912c1f22e538b02c31fa7466f0
Abhay Ramkrushna Patil <patilabhay484@gmail.com>
2025-06-07 15:21:37 +0530

    record class

--- Core java/AccessModifire.java
+++ Core java/AccessModifire.java
@@ -8,10 +8,35 @@ class C extends A{
     
 
 }
+
+// class alien{
+
+//     private final int id ;
+//     private final String name ;
+
+//     alien( int n , String str ){
+//         this.id = n ;
+//         this.name = str ;
+//     }
+    
+//     @Override 
+//     public String toString(){
+//         return id + " " + name ;
+//     }
+    
+
+// }
+
+record alien( int id , String name ){
+
+}
 class AccessModifire{
 
     public static void main(String[] args) {
         
+        alien a = new alien(0,"abhay");
+        System.out.println(a);
+
     }
 
 }
--- Core java/Race.java
+++ Core java/Race.java
@@ -1,30 +1,57 @@
-class increment{
+// // class increment{
 
-    int count = 0 ;
-    public synchronized void increment(){
+// //     int count = 0 ;
+// //     public synchronized void increment(){
 
-        for( int i = 0 ; i < 1000 ; i++ ){
-            count ++ ;
-        }
-    }
-}
+// //         for( int i = 0 ; i < 1000 ; i++ ){
+// //             count ++ ;
+// //         }
+// //     }
+// // }
 
-public class Race {
+// // public class Race {
     
-    public static void main(String[] args) {
+// //     public static void main(String[] args) throws InterruptedException {
         
-        increment i = new increment();
-        Thread t1 = new Thread(() ->{
-            i.increment();
-        });
+// //         increment i = new increment();
+// //         Thread t1 = new Thread(() ->{
+// //             i.increment();
+// //         });
         
-        Thread t2 = new Thread(() ->{
-            i.increment();
-        });
+// //         Thread t2 = new Thread(() ->{
+// //             i.increment();
+// //         });
 
-        t1.start();
-        t2.start();
-        System.out.println(i.count);
+// //         t1.start();
+// //         t2.start();
+// //         t1.join();
+// //         t2.join();
+// //         System.out.println(i.count);
 
-    }
-}
+// //         var n = 10 ;
+// //         System.out.println(n);
+// //     }
+// // }
+
+// sealed class A extends Thread permits B,C {
+
+//     A(){
+//         System.out.println("A");
+//     }
+// }
+// final class B extends A{
+
+// }
+// final class C extends A{
+
+// }
+// class D{
+
+// }
+
+// class Race{
+
+//     public static void main(String[] args ){
+//         B b = new B();
+//     }
+// }
